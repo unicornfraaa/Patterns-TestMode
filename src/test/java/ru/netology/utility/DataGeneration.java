5ee7b9457a18a8c4df0035102da5657144da640b
Ksenia <ksuskaxD@gmail.com>
2022-10-23 21:01:25 +0300

    fixed

--- src/test/java/ru/netology/utility/DataGeneration.java
+++ src/test/java/ru/netology/utility/DataGeneration.java
@@ -1,19 +1,26 @@
 package ru.netology.utility;
+
 import com.github.javafaker.Faker;
 import com.google.gson.Gson;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
+
 import java.util.Locale;
+
 import static io.restassured.RestAssured.given;
+
 public class DataGeneration {
     private DataGeneration() {
     }
+
     public static class Registration {
         private Registration() {
         }
+
         private static Faker faker = new Faker(new Locale("en"));
+
         private static RequestSpecification requestSpec = new RequestSpecBuilder()
                 .setBaseUri("http://localhost")
                 .setPort(9999)
@@ -21,6 +28,8 @@ public class DataGeneration {
                 .setContentType(ContentType.JSON)
                 .log(LogDetail.ALL)
                 .build();
+
+
         public static void sendRequest(RegistrationInfo user) {
             given() // "дано"
                     .spec(requestSpec) // указываем, какую спецификацию используем
@@ -30,18 +39,22 @@ public class DataGeneration {
                     .then() // "тогда ожидаем"
                     .statusCode(200); // код 200 OK
         }
+
         public static String getRandomLogin() {
             String login = faker.name().username();
             return login;
         }
+
         public static String getRandomPassword() {
             String password = faker.internet().password();
             return password;
         }
+
         public static RegistrationInfo getUser(String status) {
             RegistrationInfo user = new RegistrationInfo(getRandomLogin(), getRandomPassword(), status);
             return user;
         }
+
         public static RegistrationInfo getRegisteredUser(String status) {
             RegistrationInfo registeredUser = getUser(status);
             sendRequest(registeredUser);
